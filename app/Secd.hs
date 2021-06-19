5830490fe5bd782fd71098512b9ef5f3dc808627
Hirofumi SHINKE <hiro@molloy.local>
2021-06-19 20:42:17 +0900

    SECD virtual machine and test cases

--- app/Secd.hs
+++ app/Secd.hs
@@ -2,6 +2,15 @@
 
 module Secd where
 
+import System.IO.Unsafe
+
+trace :: Show a => String -> a -> a 
+trace s x = unsafePerformIO ( do
+                                putStrLn s
+                                putStrLn $ "result " ++ show x
+                                return x
+                            )
+            
 data Value = Num Int
            | VBool Bool
            | Closure [Code] Env
@@ -13,14 +22,24 @@ data Value = Num Int
 
 instance Show Value where
   show (Prim _) = "Primitive"
-  show (Num n) = show n
+  show (Num n) = "Num " ++ (show n)
   show (VBool n) = show n
-  show (Closure _ _) = "Colsure"
-  show (List xs) = show xs
-  show (Str s) = show s
-  show (Sym s) = show s
+  show (Closure cs e) =
+    "Colsure " ++ (show cs) ++ " " ++ (show e)
+  show (List xs) = "List " ++ (show xs)
+  show (Str s) = "Str " ++ (show s)
+  show (Sym s) = "Sym " ++ (show s)
   show Nil = "Nil"
 
+instance Eq Value where
+  Num x == Num y = x == y
+  Str x == Str y = x == y  
+  Sym x == Sym y = x == y
+  VBool x == VBool y = x == y
+  Nil   == Nil   = True
+  List xs == List ys = xs == ys
+  _ == _             = False
+  
 type Env   = [[Value]]
 type Stack = [Value]
 type Dump  = [Cont]
@@ -28,6 +47,7 @@ type GEnv  = [(String,Value)]
 
 data Cont = Cont3 Stack Env [Code]
           | Cont1 [Code]
+  deriving Show
 
 data Code = Ld (Int,Int)
           | Ldc Value
@@ -41,6 +61,7 @@ data Code = Ld (Int,Int)
           | Pop
           | Def String
           | Stop
+          | Dump
   deriving Show
 
 
@@ -48,7 +69,7 @@ globalEnv :: [(String,Value)]
 globalEnv = []
 
 getLVar :: Env -> Int -> Int -> Value
-getLVar = undefined
+getLVar e i j = e !! i !! j
 getGVar :: [(String,Value)] -> String -> Value
 getGVar g key = case lookup key g of
                   Just v -> v
@@ -63,12 +84,17 @@ exec g s e (Args n: c) d = exec g (List vs:s') e c d where vs = take n s
                                                            s' = drop n s
 exec g (Closure code e':List vs:s) e (App:c) d = exec g [] (vs:e') code (Cont3 s e c:d)
 exec g (v:s) e (Rtn:c) ((Cont3 s' e' c'):d) = exec g (v:s') e' c' d
+exec g (Prim func:List vs:s) e (App:c) d = exec g (func vs:s) e c d
 exec g (VBool b:s) e (Sel ct cf:c) d = if b then exec g s e ct (Cont1 c:d)
                                        else exec g s e cf (Cont1 c:d)
 exec g s e (Join:[]) (Cont1 c:d) = exec g s e c d
 exec g (v:s) e (Pop:c) d = exec g s e c d
 exec g (v:s) e (Def sym:c) d = exec g' (Sym sym:s) e c d where g' = (sym,v) : g
 exec g (v:s) e (Stop:c) d =  v
-
-
-
+exec g s e (Dump:c) d =  Str $ (dumpString s e c d)
+exec g s e c d = error $ "exec failure: " ++ (dumpString s e c d)
+                 
+dumpString s e c d =  "s=" ++ (show s) ++ "," ++ 
+                      "e=" ++ (show e) ++ "," ++ 
+                      "c=" ++ (show c) ++ "," ++                         
+                      "d=" ++ (show d)
---
+++ test/SecdSpec.hs
@@ -0,0 +1,194 @@
+
+module SecdSpec where
+
+import Secd
+import qualified System.IO.Silently as Silently
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+
+spec :: Spec
+spec = do
+  
+  describe "Stop" $
+    it "case 1" $ 
+    exec [] (Num 1:[]) [] (Stop:[]) [] `shouldBe` (Num 1)
+
+  describe "Ld" $
+    it "case 1" $ do
+    let g = []    
+    let s = []
+    let e = [[Str "a",Str "b"]]
+    let c = [Ld (0,1),Stop]
+    let d = []
+    exec g s e c d `shouldBe` (Str "b")
+
+  describe "Ldc" $ do
+    describe "String Literal" $
+      it "case 1" $ do
+      let g = []    
+      let s = []
+      let e = []
+      let c = [Ldc (Str "b"),Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Str "b")
+  
+    describe "Number Literal" $
+      it "case 2" $ do
+      let g = []    
+      let s = []
+      let e = []
+      let c = [Ldc (Num 10),Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 10)
+
+
+  describe "Ldg" $ do
+    describe "String Literal" $
+      it "case 1" $ do
+      let g = [("a",Num 1),("b",Str "b")]
+      let s = []
+      let e = []
+      let c = [Ldg "b",Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Str "b")
+
+
+  describe "Ldf" $ do
+    describe "xxxx" $
+      it "case 1" $ do
+      let g = []
+      let s = []
+      let e = [[Num 1, Num 2],[Num 3,Num 4]]
+      let c = [Ldf [Ld (0,1),Rtn],Dump]
+      let d = []
+      exec g s e c d `shouldBe` (Str $ 
+                                  "s=[Colsure [Ld (0,1),Rtn] [[Num 1,Num 2],[Num 3,Num 4]]]," ++
+                                  "e=[[Num 1,Num 2],[Num 3,Num 4]],c=[],d=[]"
+                                )
+
+  describe "Args" $ do
+    describe "xxxx" $
+      it "case 1" $ do
+      let g = []
+      let s = [Num 1, Num 2,Num 3,Num 4]
+      let e = []
+      let c = [Args 3,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (List [Num 1,Num 2,Num 3])
+
+  describe "App" $ do
+    describe "Primitive" $
+      it "case 1" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let g = []
+      let s = [Prim addFunc, List [Num 3, Num 5], Str "xxx"]
+      let e = []
+      let c = [App,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+    describe "Primitive2" $
+      it "case 2" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let g = []
+      let s = [List [Num 3, Num 5], Str "xxx"]
+      let e = []
+      let c = [Ldc (Prim addFunc),App,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+    describe "Primitive3" $
+      it "case 3" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let g = []
+      let s = [Str "xxx"]
+      let e = []
+      let c = [Ldc (Num 3), Ldc (Num 5), Args 2, Ldc (Prim addFunc),App,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+    describe "Primitive4" $
+      it "case 4" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let g = [("add", Prim addFunc)]
+      let s = [Str "xxx"]
+      let e = []
+      let c = [Ldc (Num 3), Ldc (Num 5), Args 2, Ldg "add",App,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+    describe "Closure1" $
+      it "case 1" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let closEnv = [[]]
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldc (Prim addFunc), App, Rtn ]
+      let clos = Closure closCodes closEnv
+      let g = []
+      let s = [clos, List [Num 3, Num 5], Str "xxx"]
+      let e = []
+      let c = [App,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+    describe "Closure2" $
+      it "case 2" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldg "add", App, Rtn ]
+      let g = [("add", Prim addFunc)]
+      let s = []
+      let e = []
+      let c = [Ldc (Num 3), Ldc (Num 5), Args 2, Ldf closCodes, App,Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+    describe "UserFunction" $
+      it "case 1" $ do
+      let addFunc (Num x:Num y:_) = Num (x+y)
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldg "add", App, Rtn ]
+      let g = [("add", Prim addFunc)]
+      let s = []
+      let e = []
+      let c = [Ldf closCodes, Def "userAdd",
+               Ldc (Num 3), Ldc (Num 5), Args 2, Ldg "userAdd", App, Stop ]
+      let d = []
+      exec g s e c d `shouldBe` (Num 8)
+
+
+  describe "Pop" $ 
+    it "case 1" $ do
+    let g = []
+    let s = [Num 1, Num 2, Num 3]
+    let e = []
+    let c = [Pop,Stop]
+    let d = []
+    exec g s e c d `shouldBe` (Num 2)
+    
+  describe "Def" $ 
+    it "case 1" $ do
+    let g = []
+    let s = []
+    let e = []
+    let c = [Ldc (Num 2),Def "a", Ldg "a", Stop]
+    let d = []
+    exec g s e c d `shouldBe` (Num 2)
+
+  describe "Sel&Join" $ do
+    describe "then-block" $
+      it "case 1" $ do
+      let g = []
+      let s = []
+      let e = []
+      let c = [Ldc (VBool True), Sel [Ldc (Num 1), Join] [Ldc (Num 2), Join], Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 1)
+
+    describe "else-block" $
+      it "case 2" $ do
+      let g = []
+      let s = []
+      let e = []
+      let c = [Ldc (VBool False), Sel [Ldc (Num 1), Join] [Ldc (Num 2), Join], Stop]
+      let d = []
+      exec g s e c d `shouldBe` (Num 2)
+
+    
