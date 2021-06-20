5ae2fe0763c786782911d7a02ccb98d30c85862c
Hirofumi SHINKE <hiro@molloy.local>
2021-06-20 14:50:08 +0900

    more test case

--- test/CompilerSpec.hs
+++ test/CompilerSpec.hs
@@ -32,11 +32,48 @@ spec = do
                                                         S.Ldg "+",
                                                         S.App,
                                                         S.Stop]
+
+  let lmd = lN[ SYM "lambda", lN [ SYM "n" ], SYM "n" ]
+
   describe "test case" $
     it "lambda" $ do 
-    let lmd = lN[ SYM "labmda", lN [ SYM "n" ], SYM "n" ]
+    compile lmd `shouldBe` [ S.Ldf [ S.Ld (0,0), S.Rtn ], S.Stop ]
+
+  describe "test case" $
+    it "lambda apply" $ do 
     compile ( lN[ lmd , INT 1] ) `shouldBe` [ S.Ldc (S.Num 1),
                                               S.Args 1,
                                               S.Ldf [ S.Ld (0,0), S.Rtn ],
                                               S.App,
                                               S.Stop ]
+
+  let lmd2 = lN[ SYM "lambda", lN [], INT 1, INT 2, INT 3, INT 4, INT 5 ]
+  describe "test case" $
+    it "lambda" $ do 
+    compile lmd2 `shouldBe` [ S.Ldf [ S.Ldc (S.Num 1),
+                                      S.Pop,
+                                      S.Ldc (S.Num 2),
+                                      S.Pop,
+                                      S.Ldc (S.Num 3),
+                                      S.Pop,
+                                      S.Ldc (S.Num 4),
+                                      S.Pop,
+                                      S.Ldc (S.Num 5),
+                                      S.Rtn ], S.Stop ]
+
+
+  let lmd3 = lN[ SYM "lambda", lN [ SYM "n", SYM "m" ], lN[ SYM "+", SYM "n", SYM "m" ] ]
+  describe "test case" $
+    it "lambda apply" $ do 
+    compile ( lN[ lmd3 , INT 3, INT 5] ) `shouldBe` [ S.Ldc (S.Num 3),
+                                                     S.Ldc (S.Num 5),
+                                                     S.Args 2,
+                                                     S.Ldf [ S.Ld (0,0), S.Ld (0,1), S.Args 2,
+                                                             S.Ldg "+",
+                                                             S.App,
+                                                             S.Rtn ],
+                                                     S.App,
+                                                     S.Stop ]
+  
+
+
