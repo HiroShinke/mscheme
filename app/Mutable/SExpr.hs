ca5af15e1970e72fcf11a476febf025d7562b7d3
Hirofumi SHINKE <hiro@molloy.local>
2021-07-08 21:15:09 +0900

    Mutable

---
+++ app/Mutable/SExpr.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Mutable.SExpr where
+
+import Data.IORef
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+import Error
+
+import qualified Data.HashTable.IO as H
+type HashTable k v = H.CuckooHashTable k v
+
+-- S 式の定義
+data SExpr = INT  !Integer
+           | REAL !Double
+           | SYM  String
+           | STR  String
+           | BOOL Bool
+           | CELL (IORef SExpr) (IORef SExpr)
+           | NIL
+           | PRIM ScmFunc
+           | SYNT ScmFunc
+           | CLOS SExpr LEnv
+           | MACR SExpr
+           | PRIM' SecdFunc       --- VM built-in function
+           | CLOS' [Code] Frame   --- compiled closure
+           | MACR' [Code] Frame   --- compiled macro
+           | CONT Cont [Cont]
+
+-- 等値の定義
+instance Eq SExpr where
+  INT x  == INT y  = x == y
+  REAL x == REAL y = x == y
+  SYM x  == SYM y  = x == y
+  STR x  == STR y  = x == y
+  BOOL x  == BOOL y  = x == y
+  NIL    == NIL    = True
+  CELL x xs == CELL y ys = x == y && xs == ys
+  _      == _      = False
+
+equalExpr :: SExpr -> SExpr -> IO Bool
+equalExpr (INT x) (INT y) = return $ x == y
+equalExpr (REAL x) (REAL y) = return $ x == y
+equalExpr (SYM x) (SYM y) = return $ x == y
+equalExpr (STR x) (STR y) = return $ x == y
+equalExpr (BOOL x) (BOOL y) = return $ x == y
+equalExpr NIL NIL = return True
+equalExpr (CELL x xs) (CELL y ys) =
+  (&&) <$> equalExprM x y <*> equalExprM xs ys
+  where
+    equalExprM x y = do
+      x' <- readIORef x
+      y' <- readIORef y
+      equalExpr x' y'
+equalExpr  _  _      = return False
+
+
+instance Show SExpr where
+  show (INT x)    = show x
+  show (REAL x)   = show x
+  show (SYM x)    = show x
+  show (STR x)    = show x
+  show (BOOL b)   = show b
+  show  NIL       = "()"
+  show (SYNT _)   = "<syntax>"
+  show (PRIM _)   = "<primitive>"
+  show (PRIM' _)  = "<primitive'>"
+  show (CLOS _ _) = "<closure>"
+  show xs         =
+    let xs' = showCell xs
+    in "(" ++ xs' ++ ")"
+
+instance Show (IORef SExpr) where
+  show ma = ""
+
+
+showCell xs = ""
+
+showCellIO (CELL ma md) = do
+  a <- readIORef ma
+  d <- readIORef md
+  (++) <$> showIO a  <*> case d of
+                           NIL      -> return ""
+                           PRIM _   -> return "<primitive>"
+                           PRIM' _  -> return "<primitive'>"
+                           CLOS _ _ -> return "<closure>"
+                           SYNT _   -> return "<syntax>"
+                           INT x    -> return $ " . " ++ show x
+                           REAL x   -> return $ " . " ++ show x
+                           SYM x    -> return $ " . " ++ x
+                           STR x    -> return $ " . " ++ show x
+                           _        -> (++) <$> return " " <*> showCellIO d
+showCellIO xs = showIO xs
+
+
+showIO (INT x)    = return $ show x
+showIO (REAL x)   = return $ show x
+showIO (SYM x)    = return x
+showIO (STR x)    = return $ show x
+showIO (BOOL b)   = return $ show b
+showIO NIL        = return "()"
+showIO (SYNT _)   = return "<syntax>"
+showIO (PRIM _)   = return "<primitive>"
+showIO (PRIM' _)  = return "<primitive'>"
+showIO (CLOS _ _) = return "<closure>"
+--showIO (CELL (SYM "quote") (CELL e es)) = "'" ++ (show e)
+--showIO (CELL (SYM "quasiquote") (CELL e es)) = "`" ++ (show e)
+--showIO (CELL (SYM "unquote") (CELL e es)) = "," ++ (show e)
+--showIO (CELL (SYM "unquote-splicing") (CELL e es)) = ",@" ++ (show e)
+showIO xs         = do
+  xs' <- showCellIO xs
+  return $ "(" ++ xs' ++ ")"
+
+
+cons :: SExpr -> SExpr -> IO SExpr
+cons x xs = CELL <$> newIORef x <*> newIORef xs
+  
+
+-- Scmエラーの定義
+data ScmError = ScmError String String deriving (Show, Eq)
+
+instance Error ScmError where
+  noMsg    = ScmError "" ""
+  strMsg s = ScmError  "" s
+
+type Scm a    = ExceptT ScmError IO a
+type ScmFunc  = Env  -> SExpr -> Scm SExpr
+type SecdFunc = GEnv -> SExpr -> Scm SExpr
+
+quote           = SYM "quote"
+quasiquote      = SYM "quasiquote"
+unquote         = SYM "unquote"
+unquoteSplicing = SYM "unquote-splicing"
+
+
+-- ローカル環境の定義(インタプリタ用)
+type LEnv = [(String, IORef SExpr)]
+-- グローバルな環境
+type GEnv = HashTable String SExpr
+-- 両方の環境を保持する
+type Env = (GEnv, LEnv)
+
+-- stack of frames. a frame must be list of symbol  
+type Frame = [IORef SExpr]
+type Stack = [SExpr]
+type Dump  = [Cont]
+
+type Env'  = (GEnv,Frame)
+
+data Cont = Cont3 Stack Frame [Code]
+          | Cont1 [Code]
+  deriving Show
+
+data Code = Ld (Int,Int)
+          | Ldc SExpr
+          | Ldg String
+          | Ldf [Code]
+          | Ldct [Code]
+          | Args Int
+          | ArgsAp Int
+          | App
+          | TApp
+          | Rtn
+          | Sel [Code] [Code]
+          | Selr [Code] [Code]
+          | Join
+          | Pop
+          | Def String
+          | Defm String
+          | Stop
+          | Dump
+  deriving Show
+
+
+-- 真偽値
+true  = BOOL True
+false = BOOL False
+
+debugPrintOn = False
+
+debugPrint :: String -> Scm ()
+debugPrint msg = if debugPrintOn
+                 then liftIO $ putStrLn $ msg
+                 else return ()
+
+type CompilerProc   = Env' -> SExpr -> [Code] -> Bool -> Scm [Code]
+type CompilerProc'  = Env' -> SExpr -> [Code] -> Scm [Code]
+type TranslatorProc = Env' -> SExpr -> [Code] -> Scm [Code]
+
+
---
+++ app/Mutable/Secd.hs
@@ -0,0 +1,134 @@
+
+
+module Mutable.Secd where
+
+import System.IO.Unsafe
+import Data.IORef
+import Mutable.SExpr
+import qualified Data.HashTable.IO as H
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+import Error
+
+trace :: Show a => String -> a -> a 
+trace s x = unsafePerformIO ( do
+                                putStrLn s
+                                putStrLn $ "result " ++ show x
+                                return x
+                            )
+            
+getLVar :: Frame -> Int -> Int -> IO (IORef SExpr)
+getLVar e i j = do
+  let fr = e !! i
+  if j < 0 
+    then getItem' fr (-j)
+    else do f' <- readIORef fr
+            getItem  f' j  
+  where getItem :: SExpr -> Int -> IO (IORef SExpr)
+        getItem (CELL x xs) 0 = return x
+        getItem (CELL x xs) n = do
+          xs' <- readIORef xs
+          getItem xs' (n-1)
+        getItem  NIL n        = error "getItem: can't come here"
+        getItem' :: (IORef SExpr) -> Int -> IO (IORef SExpr)
+        getItem' v 1 = return v
+        getItem' v n = do
+          v' <- readIORef v
+          case v' of
+            (CELL x xs) -> getItem' xs (n-1)
+            _           -> error "getItem': can't come here"
+
+getGVar :: GEnv -> String -> Scm SExpr
+getGVar g key = do
+  x <- liftIO $ H.lookup g key 
+  case x of
+    Just v -> return v
+    Nothing -> throwE $ strMsg $ "undefined value: " ++ key
+
+exec :: GEnv -> Stack -> Frame -> [Code] -> Dump ->  Scm SExpr
+exec g s e (Ld (i,j):c) d = do
+  v <- liftIO $ getLVar e i j
+  v' <- liftIO $ readIORef v
+  exec g (v':s) e c d
+    
+exec g s e (Ldc v:c) d = exec g (v:s) e c d
+exec g s e (Ldg sym:c) d = do
+  v <- getGVar g sym
+  exec g (v:s) e c d
+exec g s e (Ldf code: c) d = exec g (CLOS' code e:s) e c d
+exec g s e (Args n: c) d = do
+  vs <- liftIO $ listToCell (reverse $ take n s)
+  let s' = drop n s
+  exec g (vs:s') e c d
+                             
+exec g s e (ArgsAp n: c) d = do
+  vs <- liftIO $ listToCell (reverse $ take n s) >>= consOnLast
+  let s' = drop n s
+  exec g (vs:s') e c d
+  where 
+    consOnLast (CELL x xs) = do
+      xs' <- readIORef xs
+      case xs' of
+        NIL -> return NIL
+        _   -> CELL <$> (readIORef x >>= newIORef ) <*>
+                        (consOnLast xs' >>= newIORef)
+    consOnLast _           = error "bad args"
+    copySExp   (CELL x xs) = CELL <$>
+                             (readIORef x  >>= newIORef) <*>
+                             (readIORef xs >>= copySExp >>= newIORef )
+    copySExp           NIL = return NIL
+
+exec g (CLOS' code e':vs:s) e (App:c) d  = do
+  dumps <- liftIO $ dumpString s e c d
+  debugPrint $ "App :" ++ dumps
+  e'' <- liftIO $ (:) <$> newIORef vs <*> return e'
+  exec g [] e'' code (Cont3 s e c:d)
+exec g (CLOS' code e':vs:s) e (TApp:c) d = do
+  dumps <- liftIO $ dumpString s e c d
+  debugPrint $ "TApp :" ++ dumps
+  e'' <- liftIO $ (:) <$> newIORef vs  <*> return e'
+  exec g s e'' code d
+exec g (v:s) e (Rtn:c) ((Cont3 s' e' c'):d) = exec g (v:s') e' c' d
+exec g (PRIM' func:v:s) e (App:c) d  = do { v' <- func g v; exec g (v':s) e c d }
+exec g (PRIM' func:v:s) e (TApp:c) d = do { v' <- func g v; exec g (v':s) e c d }
+exec g (CONT (Cont3 s' e' c') d':((CELL v vs):s)) e (App:c) d  = do
+  v' <- liftIO $ readIORef v
+  exec g (v':s') e' c' d'
+exec g (CONT (Cont3 s' e' c') d':((CELL v vs):s)) e (TApp:c) d = do
+  v' <- liftIO $ readIORef v
+  exec g (v':s') e' c' d'
+
+exec g (BOOL b:s) e (Sel ct cf:c) d  = if b then exec g s e ct (Cont1 c:d)
+                                       else exec g s e cf (Cont1 c:d)
+exec g (BOOL b:s) e (Selr ct cf:c) d = if b then exec g s e ct d
+                                       else exec g s e cf d
+exec g s e (Join:[]) (Cont1 c:d) = exec g s e c d
+exec g (v:s) e (Pop:c) d = exec g s e c d
+exec g (v:s) e (Def sym:c) d = do liftIO $ H.insert g sym v  
+                                  exec g (SYM sym:s) e c d
+exec g (CLOS' code e':s) e (Defm sym:c) d = do liftIO $ H.insert g sym (MACR' code e')
+                                               exec g (SYM sym:s) e c d
+exec g s e (Ldct cs:c) d = exec g (CONT (Cont3 s e cs) d:s) e c d
+                           
+exec g (v:s) e (Stop:c) d =  return v
+exec g s e (Dump:c) d =  liftIO $ (dumpString s e c d >>= (return . STR) )
+exec g s e c d = do
+  dumps <- liftIO $ dumpString s e c d
+  throwE $ strMsg $ "exec failure: " ++ dumps
+                 
+dumpString :: [SExpr] -> [IORef SExpr] -> [Code] -> [Cont] -> IO String
+dumpString s e c d = do 
+  e' <- mapM readIORef e 
+  return $ "s=" ++ (show s) ++ "," ++ 
+           "e=" ++ (show e') ++ "," ++ 
+           "c=" ++ (show c) ++ "," ++                         
+           "d=" ++ (show d)
+
+listToCell :: [SExpr] -> IO SExpr
+listToCell (x:xs) = CELL <$> newIORef x <*> (listToCell xs >>= newIORef )
+listToCell []     = return NIL
+
+-- cellToList :: SExpr -> [SExpr]
+-- cellToList (CELL x xs) = x : (cellToList xs)
+-- cellToList  NIL       = []
