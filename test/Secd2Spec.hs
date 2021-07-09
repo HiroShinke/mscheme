290a12686746e4356a769a2aa77fba97acdbd948
Hirofumi SHINKE <hiro@molloy.local>
2021-07-10 08:54:33 +0900

    porting almost testcases

--- test/Secd2Spec.hs
+++ test/Secd2Spec.hs
@@ -84,15 +84,270 @@ spec = do
         case l of
           (INT x: INT y: _ ) -> return $ INT (x+y)
           _                  -> error "error"
-  
+
   describe "App" $ do
-    it "Primitive 1" $ do
+    describe "Primitive" $
+      it "case 1" $ do
       g <- H.new
-      let s = [PRIM' addFunc, lN[INT 3, INT 5], STR "xxx"]
+      let s = [PRIM' addFunc, lN [INT 3, INT 5], STR "xxx"]
       let e = []
       let c = [App,Stop]
       let d = []
       exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Primitive2" $
+      it "case 2" $ do
+      g <- H.new
+      let s = [lN [INT 3, INT 5], STR "xxx"]
+      let e = []
+      let c = [Ldc (PRIM' addFunc),App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Primitive3" $
+      it "case 3" $ do
+      g <- H.new
+      let s = [STR "xxx"]
+      let e = []
+      let c = [Ldc (INT 3), Ldc (INT 5), Args 2, Ldc (PRIM' addFunc),App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Primitive4" $
+      it "case 4" $ do
+      g <- H.fromList [("add", PRIM' addFunc)]
+      let s = [STR "xxx"]
+      let e = []
+      let c = [Ldc (INT 3), Ldc (INT 5), Args 2, Ldg "add",App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Closure1" $
+      it "case 1" $ do
+      n <- newIORef NIL
+      let closEnv = [n]
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldc (PRIM' addFunc), App, Rtn ]
+      let clos = CLOS' closCodes closEnv
+      g <- H.new
+      let s = [clos, lN [INT 3, INT 5], STR "xxx"]
+      let e = []
+      let c = [App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Closure2" $
+      it "case 2" $ do
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldg "add", App, Rtn ]
+      g <- H.fromList [("add", PRIM' addFunc)]
+      let s = []
+      let e = []
+      let c = [Ldc (INT 3), Ldc (INT 5), Args 2, Ldf closCodes, App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Closure3" $ do
+      it "list parameter 1" $ do
+        let closCodes = [Ld (0,-1), Rtn ]
+        let s = []
+        let e = []
+        let c = [Ldc (INT 1), Ldc (INT 2), Ldc (INT 3),
+                 Args 3, Ldf closCodes, App, Stop]
+        let d = []
+        g <- H.new
+        exec g s e c d `shouldBeT` lN[INT 1, INT 2, INT 3]
+
+      it "list parameter 2" $ do
+        let closCodes = [Ld (0,-2), Rtn ]
+        let s = []
+        let e = []
+        let c = [Ldc (INT 1), Ldc (INT 2), Ldc (INT 3),
+                 Args 3, Ldf closCodes, App, Stop]
+        let d = []
+        g <- H.new
+        exec g s e c d `shouldBeT` lN[INT 2, INT 3]
+
+      it "list parameter 3" $ do
+        let closCodes = [Ld (0,-3), Rtn ]
+        let s = []
+        let e = []
+        let c = [Ldc (INT 1), Ldc (INT 2), Ldc (INT 3),
+                 Args 3, Ldf closCodes, App, Stop]
+        let d = []
+        g <- H.new
+        exec g s e c d `shouldBeT` lN[INT 3]
+
+    describe "UserFunction" $
+      it "case 1" $ do
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldg "add", App, Rtn ]
+      g <- H.fromList [("add", PRIM' addFunc)]
+      let s = []
+      let e = []
+      let c = [Ldf closCodes, Def "userAdd",
+               Ldc (INT 3), Ldc (INT 5), Args 2, Ldg "userAdd", App, Stop ]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Macro" $
+      it "case 1" $ do
+      g <- H.new
+      let closCodes = [Ld (0,0), Rtn ]
+      let s = []
+      let e = []
+      let c = [Ldf closCodes, Defm "a", Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (SYM "a")
+
+  describe "TApp" $ do
+    describe "Primitive" $
+      it "same as App for primitives 1" $ do
+      g <- H.new
+      let s = [PRIM' addFunc, lN [INT 3, INT 5], STR "xxx"]
+      let e = []
+      let c = [TApp,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Primitive2" $
+      it "same as App for primitives 2" $ do
+      g <- H.new
+      let s = [lN [INT 3, INT 5], STR "xxx"]
+      let e = []
+      let c = [Ldc (PRIM' addFunc),TApp,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Primitive3" $
+      it "same as App for primitives 3" $ do
+      g <- H.new
+      let s = [STR "xxx"]
+      let e = []
+      let c = [Ldc (INT 3), Ldc (INT 5), Args 2, Ldc (PRIM' addFunc),TApp,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Primitive4" $
+      it "same as App for primitives 4" $ do
+      g <- H.fromList [("add", PRIM' addFunc)]
+      let s = [STR "xxx"]
+      let e = []
+      let c = [Ldc (INT 3), Ldc (INT 5), Args 2, Ldg "add",TApp,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Closure1" $
+      it "case 1" $ do
+      n <- newIORef NIL
+      let closEnv = [n]
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldc (PRIM' addFunc), TApp, Rtn ]
+      let clos = CLOS' closCodes closEnv
+      g <- H.new
+      let s = [clos, lN [INT 3, INT 5], STR "xxx"]
+      let e = []
+      let c = [App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "Closure2" $
+      it "case 2" $ do
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldg "add", TApp, Rtn ]
+      g <- H.fromList [("add", PRIM' addFunc)]
+      let s = []
+      let e = []
+      let c = [Ldc (INT 3), Ldc (INT 5), Args 2, Ldf closCodes, App,Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+    describe "UserFunction" $
+      it "case 1" $ do
+      let closCodes = [Ld (0,0), Ld (0,1), Args 2, Ldg "add", TApp, Rtn ]
+      g <- H.fromList [("add", PRIM' addFunc)]
+      let s = []
+      let e = []
+      let c = [Ldf closCodes, Def "userAdd",
+               Ldc (INT 3), Ldc (INT 5), Args 2, Ldg "userAdd", App, Stop ]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 8)
+
+  describe "Pop" $ 
+    it "case 1" $ do
+    g <- H.new
+    let s = [INT 1, INT 2, INT 3]
+    let e = []
+    let c = [Pop,Stop]
+    let d = []
+    exec g s e c d `shouldBeT` (INT 2)
+    
+  describe "Def" $ 
+    it "case 1" $ do
+    g <- H.new
+    let s = []
+    let e = []
+    let c = [Ldc (INT 2),Def "a", Ldg "a", Stop]
+    let d = []
+    exec g s e c d `shouldBeT` (INT 2)
+
+  describe "Sel&Join" $ do
+    describe "then-block" $
+      it "case 1" $ do
+      g <- H.new
+      let s = []
+      let e = []
+      let c = [Ldc (BOOL True), Sel [Ldc (INT 1), Join] [Ldc (INT 2), Join], Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 1)
+
+    describe "else-block" $
+      it "case 2" $ do
+      g <- H.new
+      let s = []
+      let e = []
+      let c = [Ldc (BOOL False), Sel [Ldc (INT 1), Join] [Ldc (INT 2), Join], Stop]
+      let d = []
+      exec g s e c d `shouldBeT` (INT 2)
+
+  describe "Selr&Rtn" $ do
+    describe "then-block" $
+      it "case 1" $ do
+      g <- H.new
+      let s = []
+      let e = []
+      let c = [Ldc (BOOL True), Selr [Ldc (INT 1), Rtn] [Ldc (INT 2), Rtn], Rtn]
+      let d = [Cont3 [] [] [Stop]]
+      exec g s e c d `shouldBeT` (INT 1)
+
+    describe "else-block" $
+      it "case 2" $ do
+      g <- H.new
+      let s = []
+      let e = []
+      let c = [Ldc (BOOL False), Selr [Ldc (INT 1), Rtn] [Ldc (INT 2), Rtn], Rtn]
+      let d = [Cont3 [] [] [Stop]]
+      exec g s e c d `shouldBeT` (INT 2)
+
+    -- let cscont = [Ldc (SYM "c"), Args 3, Ldg "list", App, Stop]
+    -- let cs = [Ldc (SYM "a"), Ldct cscont, Args 1,
+    --           Ldf [Ld (0,0), Def "x", Pop, Ldc (SYM "b"), Rtn], App] ++ cscont
+
+    -- describe "call/cc" $
+    --   it "save continuation" $ do
+    --   g <- H.fromList [("list",PRIM' F.list')]
+    --   let s = []
+    --   let e = []
+    --   let c = cs
+    --   let d = []
+    --   exec g s e c d `shouldBeT` lN[ SYM "a", SYM "b", SYM "c" ]
+
+    -- describe "call/cc" $
+    --   it "save continuation" $ do
+    --   g <- H.fromList [("list",PRIM' F.list')]
+    --   let s = []
+    --   let e = []
+    --   let c = cs
+    --   let d = []
+    --   runExceptT $ exec g s e c d
+    --   let cs' = [ Ldc (SYM "d"), Args 1, Ldg "x", App ]
+    --   exec g [] [] cs' [] `shouldBeT` lN[ SYM "a", SYM "d", SYM "c" ]
+      
 
 
   
